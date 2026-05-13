400551b752a5cbc837d5785e5bf4cbce84e3e100
Lenya <leonidivanov826@gmail.com>
2026-05-13 19:17:26 +0300

    The screen displaying detailed information about each astronaut has been completed.

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,35 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Леонід Іванов on 13.05.2026.
+//
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(Text(astronaut.name))
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+
+#Preview {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    return AstronautView(astronaut: astronauts["aldrin"]!)
+        .preferredColorScheme(.dark)
+}
